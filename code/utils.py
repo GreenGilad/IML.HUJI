47238ca6728fba18efb311747cbbb4f46e2c511d
GreenGilad <gilad.green@mail.huji.ac.il>
2021-01-25 17:23:05 +0200

    Refactored lab 08. Started adding code examples of SGD

--- code/utils.py
+++ code/utils.py
@@ -16,18 +16,56 @@ pio.templates["custom"] = go.layout.Template(
 pio.templates.default = "simple_white+custom"
 
 
-
-
-def dash_show(fig):
-    from jupyter_dash import JupyterDash
-    import dash_core_components as dcc
-    import dash_html_components as html
-    app = JupyterDash(__name__)
-    app.layout = html.Div([
-        dcc.Graph(id='graph', figure=fig)
-    ], style={'display': 'inline-block', 'width': '100%', "height": "100%"})
-    app.run_server(mode='inline')
+class AnimationButtons():
+    def play_scatter(frame_duration = 500, transition_duration = 300):
+        return dict(label="Play", method="animate", args=
+                    [None, {"frame": {"duration": frame_duration, "redraw": False},
+                            "fromcurrent": True, "transition": {"duration": transition_duration, "easing": "quadratic-in-out"}}])
     
+    def play(frame_duration = 1000, transition_duration = 0):
+        return dict(label="Play", method="animate", args=
+                    [None, {"frame": {"duration": frame_duration, "redraw": True},
+                            "mode":"immediate",
+                            "fromcurrent": True, "transition": {"duration": transition_duration, "easing": "linear"}}])
+    
+    def pause():
+        return dict(label="Pause", method="animate", args=
+                    [[None], {"frame": {"duration": 0, "redraw": False}, "mode": "immediate", "transition": {"duration": 0}}])
+    
+    def slider(frame_names):       
+        steps= [dict(args=[[i], dict(frame={'duration': 300, 'redraw': False}, mode="immediate", transition= {'duration': 300})],
+                           label=i+1, method="animate")
+                for i, n in enumerate(frame_names)]
+        
+        return [dict(yanchor="top", xanchor="left",
+                     currentvalue={'font': {'size': 16}, 'prefix': 'Frame: ', 'visible': True, 'xanchor': 'right'},
+                     transition={'duration': 0, 'easing': 'linear'},
+                     pad= {'b': 10, 't': 50},
+                     len=0.9, x=0.1, y=0,
+                     steps=steps)]
+
+
+custom = [[0.0, "rgb(165,0,38)"],
+                [0.1111111111111111, "rgb(215,48,39)"],
+                [0.2222222222222222, "rgb(244,109,67)"],
+                [0.3333333333333333, "rgb(253,174,97)"],
+                [0.4444444444444444, "rgb(254,224,144)"],
+                [0.5555555555555556, "rgb(224,243,248)"],
+                [0.6666666666666666, "rgb(171,217,233)"],
+                [0.7777777777777778, "rgb(116,173,209)"],
+                [0.8888888888888888, "rgb(69,117,180)"],
+                [1.0, "rgb(49,54,149)"]]
+
+def decision_surface(predict, xrange, yrange, density=120, dotted=False, colorscale=custom, showscale=True):
+    xrange, yrange = np.linspace(*xrange, density), np.linspace(*yrange, density)
+    xx, yy = np.meshgrid(xrange, yrange)
+    pred = predict(np.c_[xx.ravel(), yy.ravel()])
+
+    if dotted:
+        return go.Scatter(x=xx.ravel(), y=yy.ravel(), opacity=1, mode="markers", marker=dict(color=pred, size=1, colorscale=colorscale, reversescale=False), hoverinfo="skip", showlegend=False)
+    return go.Contour(x=xrange, y=yrange, z=pred.reshape(xx.shape), colorscale=colorscale, reversescale=False, opacity=.5, connectgaps=True, hoverinfo="skip", showlegend=False, showscale=showscale)
+
+
 
 def save_animated_gif(frames, filename, frame_duration=100):
     import gif
