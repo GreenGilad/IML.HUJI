f64108f4a59f6f419a7dedc6a8519fa121691c28
Omri Tamam <omri.tamam@cs.huji.ac.il>
2022-03-30 19:13:04 +0300

    proccess data

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date
 
 import dateutil.utils
 import pandas
@@ -16,9 +16,11 @@ import plotly.io as pio
 pio.templates.default = "simple_white"
 POSITIVE_OR_ZERO_COLS = ["yr_renovated", "floors", "sqft_basement","bathrooms"]
 POSITIVE_COLS = ["sqft_living", "price", "sqft_living15", "sqft_above", "yr_built", "sqft_lot", "sqft_lot15"]
-REDUNDANT_COLS = ["id", "lat", "long"]
+REDUNDANT_COLS = ["lat", "long"]
 DATE_TIME_FORMAT = "%Y%m%dT%H%M%S%f"
-
+MAX_ROOMS = 15
+MAX_LOT_SQRT = 1250000
+MAX_LOT_14_SQRT = 500000
 
 def load_data(filename: str):
     """
@@ -61,6 +63,11 @@ def process_houses_data_frame(df: pandas.DataFrame):
     df = df.dropna().drop_duplicates()
 
     # remove data with invalid values (except MAY_BE_NEGATIVE_COLS)
+
+    df = df[df["sqft_lot15"] < MAX_LOT_14_SQRT]
+    df = df[df["bedrooms"] < MAX_ROOMS]
+    df = df[df["sqft_lot"] < MAX_LOT_SQRT]
+
     for col in POSITIVE_COLS:
         df = df[df[col] > 0]
     for col in POSITIVE_OR_ZERO_COLS:
@@ -75,49 +82,45 @@ def process_houses_data_frame(df: pandas.DataFrame):
     for col in REDUNDANT_COLS:
         df = df.drop(col, 1)
 
-    # relative date for today in months
-    today = dateutil.utils.today()
-    df["date"] = dt["date"].apply(lambda x: (today - datetime.strptime(x, DATE_TIME_FORMAT)) // 30)
+    # merge yr_renovated and yr_built to one col
+    df['today_year'] = int(date.today().year)
+    df["yr_renovated"] = df["yr_renovated"].astype(int)
+    df["yr_built"] = df["yr_built"].astype(int)
 
-    # change discrete zipcodes to linear, set resolution to 10 units intervals (98000, 98010..98200)
-    zip = pd.get_dummies()
+    df['yr_built'] = df.apply(lambda x:
+                              x['yr_built'] if x['yr_built'] >= x["yr_renovated"] - 5 else x["yr_renovated"],
+                              axis=1)
+    df['yr_built'] = df.apply(lambda x:
+                              x['today_year'] if x['today_year'] < x['yr_built'] else x["yr_built"],
+                              axis=1)
 
-    # change first column to intercept of ones
-    dt['id'] = dt['id'].apply(lambda x: 1)
-    pd.cut()
-
-    # cs solution
-    df = df.dropna().drop_duplicates()
-
-
-
-
-
-    df["recently_renovated"] = np.where(df["yr_renovated"] >= np.percentile(df.yr_renovated.unique(), 70), 1, 0)
     df = df.drop("yr_renovated", 1)
+    df = df.drop("today_year", 1)
 
-    df["decade_built"] = (df["yr_built"] / 10).astype(int)
-    df = df.drop("yr_built", 1)
 
+    # relative date for today in months
+    # change discrete zipcodes to linear, set resolution to 10 units intervals (98000, 98010..98200)
+
+
+    today = date.today()
+    #df["date"] = dt["date"].apply(lambda x: (today - datetime.strptime(x, DATE_TIME_FORMAT).days) // 30)
+    df['date'] = df['date'].apply(
+        lambda x: (today - date(int(x[0:4]), int(x[4:6]), int(x[6:8]))).days // 30)
+
+    # change first col to intercept of ones
+    df['id'] = df['id'].apply(lambda x: 1)
+
+    # form zipcode as dummies features
     df["zipcode"] = df["zipcode"].astype(int)
     df = pd.get_dummies(df, prefix='zipcode_', columns=['zipcode'])
-    df = pd.get_dummies(df, prefix='decade_built_', columns=['decade_built'])
 
-    # Removal of outliers (Notice that there exists methods for better defining outliers
-    # but for this course this suffices
-    df = df[df["bedrooms"] < 20]
-    df = df[df["sqft_lot"] < 1250000]
-    df = df[df["sqft_lot15"] < 500000]
-
-    df.insert(0, 'intercept', 1, True)
-    return df.drop("price", 1), df.price
-
+    return df
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of housing prices dataset
-    dt = load_data("datasets/house_prices.csv")
-    process_houses_data_frame(dt)
+    df = load_data("datasets/house_prices.csv")
+    df = process_houses_data_frame(df)
     # Question 2 - Feature evaluation with respect to response
     raise NotImplementedError()
 
