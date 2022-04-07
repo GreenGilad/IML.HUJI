cea70065ff9d55e90dcb0dc42c6d437c07afc59b
AsafShul <asafshul96@gmail.com>
2022-04-07 12:29:22 +0300

    ex2 - final

--- IMLearn/learners/regressors/polynomial_fitting.py
+++ IMLearn/learners/regressors/polynomial_fitting.py
@@ -20,7 +20,7 @@ class PolynomialFitting(BaseEstimator):
         """
         super().__init__()
         self.k = k
-        self.lin_reg = LinearRegression()
+        self.lin_reg = LinearRegression(include_intercept=False)
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
