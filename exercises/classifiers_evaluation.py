b81ea9c5f4c9320d66d63e077cb15f9c892b7a4f
YuvalGabbay <yuvalgabbay3@gmail.com>
2022-04-20 15:49:54 +0300

    changes ex3

--- exercises/classifiers_evaluation.py
+++ exercises/classifiers_evaluation.py
@@ -38,10 +38,11 @@ def run_perceptron():
     """
     for n, f in [("Linearly Separable", "linearly_separable.npy"), ("Linearly Inseparable", "linearly_inseparable.npy")]:
         # Load dataset
-        raise NotImplementedError()
+        np.load(f'.. / datasets / {f}')
 
         # Fit Perceptron and record loss in each fit iteration
         losses = []
+        perceptron=Perceptron()
         raise NotImplementedError()
 
         # Plot figure of loss as function of fitting iteration
